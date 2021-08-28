4e7ebb98da6f0d5c92966d6eafe61bff68e61a20
VengerAlex <alexvenger90@gmail.com>
2021-08-28 19:42:05 +1200

    added left side

--- src/App.js
+++ src/App.js
@@ -4,14 +4,43 @@ const App = () => {
 
             <div className="overlay">
                 <div className="drawer">
-                    <h2>Cart:</h2>
-                    <div className="cart-item d-flex align-center">
-                        <img className='sm-img mr-20' src="/img/clothes/1.jpg" alt="#"/>
-                        <div className='mr-20'>
-                            <p className='mb-5'>Man's Jacket</p>
-                            <b>312$</b>
+                    <h2 className='mb-30 d-flex justify-between '>Cart:
+                        <img className=' cu-p' src="/img/btn-remove.svg" alt="remove"/>
+                    </h2>
+                    <div className="items">
+                        <div className="cart-item d-flex align-center mb-20">
+                            <img className='sm-img mr-20' src="/img/clothes/1.jpg" alt="#"/>
+                            <div className='mr-20'>
+                                <p className='mb-5'>Man's Jacket</p>
+                                <b>312$</b>
+                            </div>
+                            <img className='remove__btn ml-auto' src="/img/btn-remove.svg" alt="remove"/>
                         </div>
-                        <img className='remove__btn' src="/img/btn-remove.svg" alt="remove"/>
+                        <div className="cart-item d-flex align-center mb-20">
+                            <img className='sm-img mr-20' src="/img/clothes/1.jpg" alt="#"/>
+                            <div className='mr-20'>
+                                <p className='mb-5'>Man's Jacket</p>
+                                <b>312$</b>
+                            </div>
+                            <img className='remove__btn ml-auto' src="/img/btn-remove.svg" alt="remove"/>
+                        </div>
+                    </div>
+                    <div className="total-block">
+                        <ul className='d-block '>
+                            <li>
+                                <span>Summ:</span>
+                                <div></div>
+                                <b>2313$</b>
+                            </li>
+                            <li>
+                                <span>Taxes 4%:</span>
+                                <div></div>
+                                <b>313$</b>
+                            </li>
+                        </ul>
+                        <button className='drawer-btn'>Place an order
+                            <img src="/img/right-arrow.svg" alt="#"/>
+                        </button>
                     </div>
                 </div>
             </div>
