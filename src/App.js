79bf1af6d962d7d1a41a65ea1b38e10c2c634e73
VengerAlex <alexvenger90@gmail.com>
2021-08-28 23:10:16 +1200

    fetched data from MOCK-API

--- src/App.js
+++ src/App.js
@@ -1,150 +1,53 @@
-const App = () => {
-    return (
-        <div className="wrapper clear">
+import Header from "./components/Header/Header";
+import Drawer from "./components/Drawer/Drawer";
+import Card from "./components/Card/Card";
+import {useEffect, useState} from "react";
 
-            <div className="overlay">
-                <div className="drawer">
-                    <h2 className='mb-30 d-flex justify-between '>Cart:
-                        <img className=' cu-p' src="/img/btn-remove.svg" alt="remove"/>
-                    </h2>
-                    <div className="items">
-                        <div className="cart-item d-flex align-center mb-20">
-                            <img className='sm-img mr-20' src="/img/clothes/1.jpg" alt="#"/>
-                            <div className='mr-20'>
-                                <p className='mb-5'>Man's Jacket</p>
-                                <b>312$</b>
-                            </div>
-                            <img className='remove__btn ml-auto' src="/img/btn-remove.svg" alt="remove"/>
-                        </div>
-                        <div className="cart-item d-flex align-center mb-20">
-                            <img className='sm-img mr-20' src="/img/clothes/1.jpg" alt="#"/>
-                            <div className='mr-20'>
-                                <p className='mb-5'>Man's Jacket</p>
-                                <b>312$</b>
-                            </div>
-                            <img className='remove__btn ml-auto' src="/img/btn-remove.svg" alt="remove"/>
-                        </div>
-                    </div>
-                    <div className="total-block">
-                        <ul className='d-block '>
-                            <li>
-                                <span>Summ:</span>
-                                <div></div>
-                                <b>2313$</b>
-                            </li>
-                            <li>
-                                <span>Taxes 4%:</span>
-                                <div></div>
-                                <b>313$</b>
-                            </li>
-                        </ul>
-                        <button className='drawer-btn'>Place an order
-                            <img src="/img/right-arrow.svg" alt="#"/>
-                        </button>
-                    </div>
+
+
+const App = () => {
+const [cartOpened, setCartOpened] = useState(false);
+const [items, setItems] = useState([]);
+
+useEffect(() => {
+    fetchData()
+}, [])
+
+const fetchData = async () => {
+    const res = await fetch('https://612a905922bb490017893971.mockapi.io/items');
+    const data = await res.json();
+
+    setItems(data);
+}
+
+
+return (
+    <div className="wrapper clear">
+        {cartOpened && <Drawer onCloseCart={() => setCartOpened(false)}/>}
+        <Header onClickCart={() => setCartOpened(true)}/>
+        <div className="content p-40">
+            <div className='d-flex justify-between align-center mb-40'>
+                <h1>All products</h1>
+                <div className="search-block d-flex">
+                    <img src="/img/search.svg" alt="search"/>
+                    <input type="text" placeholder='Search'/>
                 </div>
             </div>
-
-            <header className="header d-flex justify-between p-20 align-center p-40">
-                <div className='d-flex align-center'>
-                    <img className='header__logo mr-20' src='/img/logo.svg' alt="logo"/>
-                    <div>
-                        <h3 className='text-uppercase'>Internet Shop</h3>
-                        <p>Everything you need!</p>
-                    </div>
-                </div>
-                <ul className='d-flex'>
-                    <li className='mr-30'>
-                        <img className='header__svg header__svg-cart' src='/img/cart.svg' alt="logo"/>
-                        <span>132$</span>
-                    </li>
-                    <li>
-                        <img className='header__svg header__svg-user' src='/img/user.svg' alt="logo"/>
-                    </li>
-                </ul>
-            </header>
-            <div className="content p-40">
-                <div className='d-flex justify-between align-center mb-40'>
-                    <h1>All products</h1>
-                    <div className="search-block d-flex">
-                        <img src="/img/search.svg" alt="search"/>
-                        <input type="text" placeholder='Search'/>
-                    </div>
-                </div>
-                <div className="d-flex">
-                    <div className="card">
-                        <div className='cloth'>
-                            <img src="/img/clothes/1.jpg" alt="Clothes"/>
-                        </div>
-                        <h5 className='card__title'>Man's Jacket</h5>
-                        <div className="d-flex justify-between align-center">
-                            <div className='d-flex flex-column'>
-                                <b>321$</b>
-                            </div>
-                            <div className='d-flex'>
-                                <img className='mr-10 fav' src="/img/fav-after.svg" alt="#"/>
-                                <button className='card__btn'>
-                                    <img width={11} height={11} src="/img/add.svg" alt=""/>
-                                </button>
-                            </div>
-
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className='cloth'>
-                            <img src="/img/clothes/2.jpg" alt="Clothes"/>
-                        </div>
-                        <h5 className='card__title'>T-Shirt</h5>
-                        <div className="d-flex justify-between align-center">
-                            <div className='d-flex flex-column'>
-                                <b>222$</b>
-                            </div>
-                            <div className='d-flex'>
-                                <img className='mr-10 fav' src="/img/fav-before.svg" alt="#"/>
-                                <button className='card__btn'>
-                                    <img width={11} height={11} src="/img/add.svg" alt=""/>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className='cloth'>
-                            <img src="/img/clothes/3.jpg" alt="Clothes"/>
-                        </div>
-                        <h5 className='card__title'>Nike AirMax D321</h5>
-                        <div className="d-flex justify-between align-center">
-                            <div className='d-flex flex-column'>
-                                <b>321$</b>
-                            </div>
-                            <div className='d-flex'>
-                                <img className='mr-10 fav' src="/img/fav-before.svg" alt="#"/>
-                                <button className='card__btn '>
-                                    <img width={11} height={11} src="/img/add.svg" alt=""/>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className='cloth'>
-                            <img src="/img/clothes/4.jpg" alt="Clothes"/>
-                        </div>
-                        <h5 className='card__title'>Man's Jacket</h5>
-                        <div className="d-flex justify-between align-center">
-                            <div className='d-flex flex-column'>
-                                <b>121$</b>
-                            </div>
-                            <div className='d-flex'>
-                                <img className='mr-10 fav' src="/img/fav-before.svg" alt="#"/>
-                                <button className='card__btn'>
-                                    <img width={11} height={11} src="/img/add.svg" alt=""/>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div className="card-wrapper d-flex">
+                {items.map((el) =>
+                    (<Card
+                        key={el.id}
+                        name={el.name}
+                        imageURL={el.imageURL}
+                        price={el.price}
+                        onPlus={() => console.log('Add plus')}
+                        onFavourite={() => console.log('Add to bookmarks')}
+                    />)
+                )}
             </div>
         </div>
-    )
+    </div>
+)
 }
 
 export default App;
